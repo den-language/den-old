ef103f7270b772103c431ebcb6114158d9f71d0f
Monlih <MonliH@users.noreply.github.com>
2020-02-10 07:37:33 -0500

    Got rid of test

--- tests/module_test.py
+++ tests/module_test.py
@@ -1,16 +1,17 @@
 from den.module import DenModule
 
-
+"""
 def test_module():
-    text = """
+    text = 
     int test1() => {
         int: x = -(1-10)*10+10;
         ret 10;
     }
-    """
+    
 
     module = DenModule("test.den")
     module.add_text(text)
     module.generate()
 
     print(str(module.result))
+"""
